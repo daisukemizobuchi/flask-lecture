d5a681894de7c05cde9ba9238f9a0e53ba802ec6
Gakufumi Tahara <gaku@hanahanaworks.com>
2020-07-29 00:21:21 +0900

    テンプレートまでやったよ

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,24 @@ def hello_world():
 @app.route('/top')
 def top():
     return "ここはトップだよ"
+
+@app.route('/hello/<text>')
+def namehello(text):
+    return text + "さんこんにちは"
+
+@app.route('/index')
+def index():
+    name="daisuke"
+    age=20
+    address="田町"
+    return render_template('index.html',user_name = name,user_address=address,user_age=age)
+
+@app.route('/weather')
+def weather():
+    tenki="晴れ"
+    return render_template('weather.html',user_tenki=tenki)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
